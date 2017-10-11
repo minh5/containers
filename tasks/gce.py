4f031e7cb56cee7deb1aef2aa96afde402957285
Minh Mai <minh.mai@cava.com>
2017-10-11 11:53:44 -0400

    added more gce and mk invokes

--- tasks/gce.py
+++ tasks/gce.py
@@ -3,19 +3,22 @@ import os
 from invoke import task
 
 DEFAULTS = {
-    'PROJECT': os.environ.get('GCE_PROJ_ID'),
+    'PROJECT': os.environ.get('GCE_PROJ'),
     'ZONE': os.environ.get('GCE_ZONE'),
     'N_NODES': 4
 }
 
 @task
-def start(ctx, machine='n1-standard-1'):
+def start(ctx, machine='n1-standard-2'):
     cmd = """
     gcloud alpha container clusters create instances \
         --machine-type "{machine}" \
         --num-nodes "{nnodes}" \
         --zone {zone} \
-        --project "{project}"
+        --project "{project}" \
+        --enable-autoscaling \
+        --min-nodes 1 \
+        --max-nodes 8 \
         --preemptible
     """.format(
         machine=machine,
@@ -25,6 +28,9 @@ def start(ctx, machine='n1-standard-1'):
     )
     ctx.run(cmd, echo=True)
 
+def get_instance_group(ctx):
+    stdout = ctx.run('gcloud compute instance-groups list').stdout
+    return stdout.split('\n')[1].split()[0] 
 
 @task
 def add_to_firewall(ctx):
@@ -32,4 +38,5 @@ def add_to_firewall(ctx):
 
 @task
 def stop(ctx):
-    pass
+    grp = get_instance_group(ctx)
+    ctx.run('gcloud compute instance-groups managed delete %s' % grp, echo=True)
